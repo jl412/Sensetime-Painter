31aa45614d1747bee0647d2aec89a449ee5f8864
j_l <liqiangjohnny@gmail.com>
2016-03-18 16:53:05 +0800

    UI updates

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-// function moveRight() {
-//     // Hide to left / show from left
-//     $("#page1").hide("slide", {direction: "left"}, 500);
 
-//     // Show from right / hide to right
-//     $("#page2").show("slide", {direction: "right"}, 500);
-// }
+function snapContent() {
+    if ($('.page-btn-wrapper').css("height") == "140px") {
+        $('#page0 .content-text').addClass("snap-left");
+    }else{
+        $('#page0 .content-text').removeClass("snap-left");
+    }
+}
 
-function moveLeft(){
+function moveLeft(pageToShow){
 	var pageToShow = "page" + (parseInt($(this).parents(".page:first").attr("value")) - 1);
     // Hide to left / show from left
     $(this).parents(".page:first").hide("slide", {direction: "right"}, 500);
@@ -25,7 +26,25 @@ function moveRight() {
 }
 
 
+function showImg() {
+    if($('#page0').is(':visible')) {
+            var time = 0;
+            $('.fadeChain').each(function() {
+                $(this)
+                    .delay(time)
+                    .css({"visibility":"visible"})
+                    .animate({opacity: "1", top: "0"}, 500, "swing");
+                time += 300;
+            });
+    }
+}
+
+snapContent();
+showImg();
+
+
 $('.next-step').click(moveRight);
 $('.back-step').click(moveLeft);
+$('.back-step').click(showImg);
 
-$('#page0').load($('.content-img')).show("slide", {direction: "right"}, 500);
+$( window ).resize(snapContent);
