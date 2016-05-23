fe573662a634203673cf9d3fd1590ccbb59a5068
lihaolin <lihaolin@sensetime.com>
2016-05-23 10:20:55 +0800

    login

--- js/accounts.js
+++ js/accounts.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
 
             // var pattern = /^[A-Za-z][A-Za-z0-9_]+$/;
             // if(!username || username.length<4 || username.length>50 || !username.match(pattern)) {
-            //     $("#tip div").text("用户名为4~50位, 只能输入字母、下划线、数字，必须以字母开头.");
-            //     $("#tip").show();
+            //     $(".tip div").text("用户名为4~50位, 只能输入字母、下划线、数字，必须以字母开头.");
+            //     $(".tip").show();
             //     return;
             // }
 
             // if(!password || password.length<6 || password.length>50){
-            //     $("#tip div").text("密码长度须为6~50位!");
-            //     $("#tip").show();
+            //     $(".tip div").text("密码长度须为6~50位!");
+            //     $(".tip").show();
             //     return;
             // }
 
@@ -31,13 +31,13 @@ $(document).ready(function () {
                     if(result.success){
                         window.location.href="/index";
                     }else{
-                        $("#tip div").text(result.msg);
-                        $("#tip").show();
+                        $(".tip div").text(result.msg);
+                        $(".tip").show();
                     }
                 },
                 error : function() {
-                    $("#tip div").text("登录请求出错!");
-                    $("#tip").show();
+                    $(".tip div").text("登录请求出错!");
+                    $(".tip").show();
                 }
             });
     });
@@ -49,14 +49,14 @@ $(document).ready(function () {
 
             // var pattern = /^[A-Za-z][A-Za-z0-9_]+$/;
             // if(!username || username.length<4 || username.length>50 || !username.match(pattern)) {
-            //     $("#tip div").text("用户名为4~50位, 只能输入字母、下划线、数字，必须以字母开头.");
-            //     $("#tip").show();
+            //     $(".tip div").text("用户名为4~50位, 只能输入字母、下划线、数字，必须以字母开头.");
+            //     $(".tip").show();
             //     return;
             // }
 
             // if(!password || password.length<6 || password.length>50){
-            //     $("#tip div").text("密码长度须为6~50位!");
-            //     $("#tip").show();
+            //     $(".tip div").text("密码长度须为6~50位!");
+            //     $(".tip").show();
             //     return;
             // }
 
@@ -74,13 +74,13 @@ $(document).ready(function () {
                     if(result.success){
                         window.location.href="/index";
                     }else{
-                        $("#tip div").text(result.msg);
-                        $("#tip").show();
+                        $(".tip div").text(result.msg);
+                        $(".tip").show();
                     }
                 },
                 error : function() {
-                    $("#tip div").text("登录请求出错!");
-                    $("#tip").show();
+                    $(".tip div").text("登录请求出错!");
+                    $(".tip").show();
                 }
             });
     });
